86c0200c1629a5d991e54a3b3a667f4cbdcb6273
Marco <m.rotondi@ngi.it>
2016-09-14 22:51:36 +0200

    Init Version

---
+++ src/test/java/com/marco/MybookingApplicationTests.java
@@ -0,0 +1,16 @@
+package com.marco;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MybookingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
