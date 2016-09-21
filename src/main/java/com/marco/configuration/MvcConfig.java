2c180f348fdcc4c9722e2f43c8639281f48184f5
Marco <m.rotondi@ngi.it>
2016-09-21 23:51:40 +0200

    Fix layout and add Security extension for html

--- src/main/java/com/marco/configuration/MvcConfig.java
+++ src/main/java/com/marco/configuration/MvcConfig.java
@@ -3,9 +3,11 @@
  */
 package com.marco.configuration;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.extras.springsecurity4.dialect.SpringSecurityDialect;
 
 /**
  * @author Marco
@@ -22,5 +24,10 @@ public class MvcConfig extends WebMvcConfigurerAdapter {
 		registry.addViewController("/").setViewName("index");
 		registry.addViewController("/login.html").setViewName("login");
 		registry.addViewController("/admin/adminPanel.html").setViewName("admin/adminPanel");
+	}
+
+	@Bean
+	public SpringSecurityDialect springSecurityDialect() {
+		return new SpringSecurityDialect();
 	}
 }
--- src/main/java/com/marco/configuration/WebSecurityConfig.java
+++ src/main/java/com/marco/configuration/WebSecurityConfig.java
@@ -33,8 +33,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 		.failureUrl("/login.html?error=true")
 		.permitAll()
 		.and()
-		.logout().permitAll()
-		.logoutRequestMatcher(new AntPathRequestMatcher("/logout.html"))
+		.logout()
+		.logoutRequestMatcher(new AntPathRequestMatcher("/admin/logout.html"))
 		.invalidateHttpSession(Boolean.TRUE)
 		.logoutSuccessUrl("/");
 	}
