913323549d93a23ca9f997271c4861ad7eb10ca5
Marco <m.rotondi@ngi.it>
2016-10-01 00:47:37 +0200

    Fix pagination and sorting columns

--- src/main/resources/static/js/appAdmin.js
+++ src/main/resources/static/js/appAdmin.js
@@ -31,23 +31,23 @@ var appAdmin = angular.module("appAdmin", ['ngResource', 'ngTable']);
 	appAdmin.controller('adminChangeController', ['$scope', '$http', function($scope, $http) {
 	}]);
 
-	appAdmin.controller("adminTableController", ['$scope', '$http', 'NgTableParams', function($scope, $http, NgTableParams){
+	appAdmin.controller("adminTableController", ['$scope', '$http', '$filter', 'NgTableParams', function($scope, $http, $filter, NgTableParams){
 		$scope.$on("addedNewResource", function(e, newResource){
 			$scope.tableParams.reload();
 	    });
 		
-	    $scope.tableParams = new NgTableParams(
-	      {
-	        page: 1,            // show first page
-	        count: 10,           // count per page
-	        sorting: {name:'asc'}
-	      },
-	      {
-	        total: 0, // length of data
+	    $scope.tableParams = new NgTableParams({ 
+	    	sorting: {
+	            id: 'asc'     
+	        }
+	    },{
+	    	total: 0,
 	        getData: function(params) {
 	        	return $http.get('/admin/api/resources.json').then(function(response){
-	        		params.total(response.data.lenght);
-	        		return response.data;
+	        		params.total(response.data.length);
+	        		var rows = $filter('orderBy')(response.data, params.orderBy());
+	        		rows = rows.slice((params.page() - 1) * params.count(), params.page() * params.count());
+	        		return rows;
 	        	}, function(response) {
 	        		console.error(response);
 	        	});
