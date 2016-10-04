4b47cfd0d4a6842d927feceb0e7a80b3c9c0c843
Marco <m.rotondi@ngi.it>
2016-10-05 00:31:43 +0200

    Refactor: port all admin to one controller

--- src/main/resources/static/js/appAdmin.js
+++ src/main/resources/static/js/appAdmin.js
@@ -6,38 +6,31 @@ var appAdmin = angular.module("appAdmin", ['ngResource', 'ngTable']);
 
 (function() {
 	/* Controller */
-	appAdmin.controller("adminAddController", ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
-		$scope.addChangeResource = function() {	
-			var data = 'description=' + $scope.description + '&type=' + $scope.type;
-			var config = {
-				headers : {
-					'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
-                }
-	        }
-			
-			$http.post('/admin/crudResource', data, config).then(function(response){
-				$rootScope.$broadcast("addChangeResource", data);
-				$('div.add').addClass('displayNone');	
-				$('button.add').removeClass('active');
-			}, function(response){
-				alert(response);
-			});
-			
-			// Making the fields empty
-			$scope.description='';
-			$scope.type='';
-		}
-	}]);
-
-	appAdmin.controller('adminChangeController', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
-		$scope.$on("changeResource", function(e, resource) {
-			$('button.change').click();
-			$scope.description = resource.description;
-			$scope.type = resource.type;
-			$scope.id = resource.id;
-			
-			$scope.addChangeResource = function () {
-				var data = 'id=' + $scope.id + '&description=' + $scope.description + '&type=' + $scope.type;
+	appAdmin.controller("adminResourceController", ['$scope', '$http', '$rootScope', '$filter', 'NgTableParams', 
+		function($scope, $http, $rootScope, $filter, NgTableParams) {
+			$scope.add = false;
+			$scope.change = false;
+			$scope.tableParams = new NgTableParams({ 
+		    	sorting: {
+		            id: 'asc'     
+		        }
+		    },{
+		    	total: 0,
+		        getData: function(params) {
+		        	return $http.get('/admin/api/resources.json').then(function(response){
+		        		params.total(response.data.length);
+		        		var rows = $filter('orderBy')(response.data, params.orderBy());
+		        		rows = rows.slice((params.page() - 1) * params.count(), params.page() * params.count());
+		        		return rows;
+		        	}, function(response) {
+		        		console.error(response);
+		        	});
+		        }
+		    });
+		
+			$scope.addChangeResource = function() {	
+				var id = ($scope.id == undefined ? "" : $scope.id);
+				var data = 'id=' + id + '&description=' + $scope.description + '&type=' + $scope.type;
 				var config = {
 					headers : {
 						'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
@@ -46,46 +39,23 @@ var appAdmin = angular.module("appAdmin", ['ngResource', 'ngTable']);
 				
 				$http.post('/admin/crudResource', data, config).then(function(response){
 					$rootScope.$broadcast("addChangeResource", data);
-					$('div.change').addClass('displayNone');	
-					$('button.change').removeClass('active');
+					$('div.add').addClass('displayNone');	
+					$('button.add').removeClass('active');
 				}, function(response){
 					alert(response);
 				});
 				
 				// Making the fields empty
-				$scope.description='';
-				$scope.type='';
+				$scope.description = '';
+				$scope.type = '';
+				$scope.id = ''
 			}
-		});
-	}]);
-
-	appAdmin.controller("adminTableController", ['$scope', '$http', '$filter', '$rootScope', 'NgTableParams', function($scope, $http, $filter, 
-			$rootScope, NgTableParams){
 		
-		$scope.$on("addChangeResource", function(e, newResource) {
-			$scope.tableParams.reload(); 
-	    });
-		
-	    $scope.tableParams = new NgTableParams({ 
-	    	sorting: {
-	            id: 'asc'     
-	        }
-	    },{
-	    	total: 0,
-	        getData: function(params) {
-	        	return $http.get('/admin/api/resources.json').then(function(response){
-	        		params.total(response.data.length);
-	        		var rows = $filter('orderBy')(response.data, params.orderBy());
-	        		rows = rows.slice((params.page() - 1) * params.count(), params.page() * params.count());
-	        		return rows;
-	        	}, function(response) {
-	        		console.error(response);
-	        	});
-	        }
-	    });
-	    
-	    $scope.editRow = function(row) {
-	    	$rootScope.$broadcast("changeResource", row);
-	    }
+			$scope.editRow = function(row) {
+				$scope.id = row.id;
+				$scope.description = row.description;
+				$scope.type = row.type;
+				$scope.change = true;
+			}
 	}]);
 })();
