9aa174c68c3f40a7a6213e8f4708a3f08b7dd13e
marcorotondi <m.rotondi@ngi.it>
2016-11-10 16:19:04 +0100

    Add Password With BCrypt

--- src/main/java/com/marco/configuration/WebSecurityConfig.java
+++ src/main/java/com/marco/configuration/WebSecurityConfig.java
@@ -4,11 +4,14 @@
 package com.marco.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 /**
@@ -22,7 +25,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable();
-
 
 		http.authorizeRequests()
 		.antMatchers("/").permitAll()
@@ -41,6 +43,15 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-		auth.inMemoryAuthentication().withUser("admin").password("admin000").roles("ADMIN");
+		auth.inMemoryAuthentication()
+		.passwordEncoder(passwordEncoder())
+		.withUser("admin")
+		.password("$2a$08$jHTQchEwkWd19VKhEQTM5.Zy6YpNiPsIDATuLZ0GTb5q1ZYVu07my")
+		.roles("ADMIN");
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 }
