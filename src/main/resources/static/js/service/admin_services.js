6bc681b0297f01e64d97fbaaa5f34ffb677c55a6
marcorotondi <m.rotondi@ngi.it>
2016-12-02 08:47:14 +0100

    Small rework

--- src/main/resources/static/js/service/admin_services.js
+++ src/main/resources/static/js/service/admin_services.js
@@ -8,70 +8,62 @@
 		var REST_SERVICE_URI = '/admin/api/';
 		 
 	    var factory = {
-	        fetchAllResource: fetchAllResource,
-	        createUpdateResource: createUpdateResource,
-	        deleteResource: deleteResource,
-	        summary: summary
+	        fetchAllResource: function () {
+		        var deferred = $q.defer();
+		        
+		        $http.get(REST_SERVICE_URI + "resources.json").then(
+		            function (response) {
+		                deferred.resolve(response.data);
+		            },
+		            function(errResponse){
+		                console.error('Error while fetching Resource');
+		                deferred.reject(errResponse);
+		            }
+		        );
+		        return deferred.promise;
+		    },
+	        createUpdateResource: function (resource) {
+		        var deferred = $q.defer();
+		        $http.post(REST_SERVICE_URI + "crudResource", resource).then(
+		            function (response) {
+		                deferred.resolve(response.data);
+		            },
+		            function(errResponse){
+		                console.error('Error while creating Resource');
+		                deferred.reject(errResponse);
+		            }
+		        );
+		        return deferred.promise;
+		    },
+	        deleteResource: function (id) {
+		        var deferred = $q.defer();
+		        $http.delete(REST_SERVICE_URI + "/delete/resource/" + id).then(
+		            function (response) {
+		                deferred.resolve(response.data);
+		            },
+		            function(errResponse){
+		                console.error('Error while deleting Resource');
+		                deferred.reject(errResponse);
+		            }
+		        );
+		        return deferred.promise;
+		    },
+	        summary: function () {
+		    	var deferred = $q.defer();
+		    	$http.get(REST_SERVICE_URI + "summary.json").then(
+	    			function (response) {
+		                deferred.resolve(response.data);
+		            },
+		            function(errResponse){
+		                console.error('Error while fetching Resource');
+		                deferred.reject(errResponse);
+		            }
+			    );
+			    return deferred.promise;
+		    }
 	    };
 	 
 	    return factory;
-	 
-	    function fetchAllResource() {
-	        var deferred = $q.defer();
-	        
-	        $http.get(REST_SERVICE_URI + "resources.json").then(
-	            function (response) {
-	                deferred.resolve(response.data);
-	            },
-	            function(errResponse){
-	                console.error('Error while fetching Resource');
-	                deferred.reject(errResponse);
-	            }
-	        );
-	        return deferred.promise;
-	    }
-	 
-	    function createUpdateResource(resource) {
-	        var deferred = $q.defer();
-	        $http.post(REST_SERVICE_URI + "crudResource", resource).then(
-	            function (response) {
-	                deferred.resolve(response.data);
-	            },
-	            function(errResponse){
-	                console.error('Error while creating Resource');
-	                deferred.reject(errResponse);
-	            }
-	        );
-	        return deferred.promise;
-	    }
-	 
-	    function deleteResource(id) {
-	        var deferred = $q.defer();
-	        $http.delete(REST_SERVICE_URI + "/delete/resource/" + id).then(
-	            function (response) {
-	                deferred.resolve(response.data);
-	            },
-	            function(errResponse){
-	                console.error('Error while deleting Resource');
-	                deferred.reject(errResponse);
-	            }
-	        );
-	        return deferred.promise;
-	    }
-	    
-	    function summary() {
-	    	var deferred = $q.defer();
-	    	$http.get(REST_SERVICE_URI + "summary.json").then(
-    			function (response) {
-	                deferred.resolve(response.data);
-	            },
-	            function(errResponse){
-	                console.error('Error while fetching Resource');
-	                deferred.reject(errResponse);
-	            }
-		    );
-		    return deferred.promise;
-	    }
 	}]);
 	
 	appAdmin.factory('MessageService', ['$rootScope', function($rootScope){
--- src/main/resources/static/js/service/scheduler_service.js
+++ src/main/resources/static/js/service/scheduler_service.js
@@ -8,75 +8,71 @@
 		var REST_SERVICE_URI = '/public/api/';
 		
 		var appointmentSource = {
-	        	async: false,
-	        	dataType: "json", 
-	            dataFields: [
-	                { name: 'id', type: 'string' },
-	                { name: 'description', type: 'string' },
-	                { name: 'location', type: 'string' },
-	                { name: 'subject', type: 'string' },
-	                { name: 'calendar', type: 'string' },
-	                { name: 'start', type: 'date' },
-	                { name: 'end', type: 'date' },
-	                { name: 'draggable', type: 'boolean'},
-	                { name: 'resizable', type: 'boolean'}
-	            ],
-	            id: 'id',
-	            url: "/public/api/schedulers",
-	        };
+        	async: false,
+        	dataType: "json", 
+            dataFields: [
+                { name: 'id', type: 'string' },
+                { name: 'description', type: 'string' },
+                { name: 'location', type: 'string' },
+                { name: 'subject', type: 'string' },
+                { name: 'calendar', type: 'string' },
+                { name: 'start', type: 'date' },
+                { name: 'end', type: 'date' },
+                { name: 'draggable', type: 'boolean'},
+                { name: 'resizable', type: 'boolean'}
+            ],
+            id: 'id',
+            url: "/public/api/schedulers",
+        };
 		
 		var resourceSource = {
-            	async: false,
-            	dataType: "json", 
-                dataFields: [
-                    { name: 'id', type: 'string' },
-                    { name: 'description', type: 'string' },
-                    { name: 'location', type: 'string' },
-                    { name: 'subject', type: 'string' },
-                    { name: 'calendar', type: 'string' }
-                ],
-                id: 'id',
-                url: "/public/api/resources",
-            };
+        	async: false,
+        	dataType: "json", 
+            dataFields: [
+                { name: 'id', type: 'string' },
+                { name: 'description', type: 'string' },
+                { name: 'location', type: 'string' },
+                { name: 'subject', type: 'string' },
+                { name: 'calendar', type: 'string' }
+            ],
+            id: 'id',
+            url: "/public/api/resources",
+        };
 		
 		var factory = {
-		        appointment: appointment,
-		        deleteAppointment: deleteAppointment,
+		        appointment: function (schedulerData) {
+					var deferred = $q.defer();
+			        
+					$http.post(REST_SERVICE_URI + "appointment/create", schedulerData).then(
+			            function (response) {
+			                deferred.resolve(response.data);
+			            },
+			            function(errResponse){
+			                console.error(errResponse.data.errorMessage);
+			                deferred.reject(errResponse);
+			            }
+			        );
+			        return deferred.promise;
+				},
+		        deleteAppointment: function (appointmentId, checkCode) {
+					var deferred = $q.defer();
+					var parameterUrl = appointmentId + "/" + checkCode;
+			        
+					$http.delete(REST_SERVICE_URI + "appointment/delete/" + parameterUrl, {}).then(
+			            function (response) {
+			                deferred.resolve(response.data);
+			            },
+			            function(errResponse){
+			                console.error('Error while deleting Appointment');
+			                deferred.reject(errResponse);
+			            }
+			        );
+			        return deferred.promise;
+				},
 		        appointmentSource: appointmentSource,
 		        resourceSource: resourceSource
 		};
-		 
+		
 		return factory;
-		
-		function appointment(schedulerData) {
-			var deferred = $q.defer();
-	        
-			$http.post(REST_SERVICE_URI + "appointment/create", schedulerData).then(
-	            function (response) {
-	                deferred.resolve(response.data);
-	            },
-	            function(errResponse){
-	                console.error(errResponse.data.errorMessage);
-	                deferred.reject(errResponse);
-	            }
-	        );
-	        return deferred.promise;
-		}
-		
-		function deleteAppointment(appointmentId, checkCode) {
-			var deferred = $q.defer();
-			var parameterUrl = appointmentId + "/" + checkCode;
-	        
-			$http.delete(REST_SERVICE_URI + "appointment/delete/" + parameterUrl, {}).then(
-	            function (response) {
-	                deferred.resolve(response.data);
-	            },
-	            function(errResponse){
-	                console.error('Error while deleting Appointment');
-	                deferred.reject(errResponse);
-	            }
-	        );
-	        return deferred.promise;
-		}
 	}]);
 })();
