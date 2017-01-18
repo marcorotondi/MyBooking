d65311b0f7f4d506e7b60ab7fc3a09dc378ebdb7
marcorotondi <m.rotondi@ngi.it>
2017-01-18 11:41:59 +0100

    Fix Import

--- src/test/java/com/marco/MybookingApplicationTests.java
+++ src/test/java/com/marco/MybookingApplicationTests.java
@@ -4,7 +4,6 @@ import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
