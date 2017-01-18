6f23bfaf524be7584d292be66924b5896a05b2cd
marcorotondi <m.rotondi@ngi.it>
2017-01-18 11:41:20 +0100

    Fix import

--- src/test/java/com/marco/MyBookingAdminControllerTests.java
+++ src/test/java/com/marco/MyBookingAdminControllerTests.java
@@ -13,7 +13,6 @@ import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
